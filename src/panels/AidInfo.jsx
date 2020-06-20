a1a12f4b5433ef2b66affc7a9eefdd30ddcdbcb1
Maba <azraiths@gmail.com>
2020-06-20 15:09:04 +0300

    Finished: AidInfo

--- src/panels/AidInfo.jsx
+++ src/panels/AidInfo.jsx
@@ -1,40 +1,82 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
-  Footer,
-  FormLayout, FormLayoutGroup, Select, Separator, Text,
+  Cell,
+  FormLayout, FormLayoutGroup, List, Separator, Text,
 } from '@vkontakte/vkui';
 
-// TODO: Заменить Text на правильный элемент
 const AidInfo = (props) => {
-  const { name, type } = props;
+  const {
+    user_id, medical, type, doctor, dstart, dfinish, times,
+  } = props;
 
   const style = {
-    label: {
+    topLabel: {
       width: '100%',
       padding: '0 11.5px',
       display: 'inline-block',
     },
     separator: {
-      margin: '0 11.5px',
+      margin: '0 1px',
+    },
+
+    mainLabel: {
+      width: '100%',
+      padding: '0 11.5px',
+      display: 'inline-block',
+    },
+
+    timeCard: {
+      time: {
+        fontWeight: 'bold',
+        color: 'black',
+      },
+      dose: {
+        paddingLeft: '10px',
+      },
     },
   };
 
   return (
     <FormLayout>
       <FormLayoutGroup top="Название">
-        <Text style={style.label}>{name}</Text>
+        <Text style={style.topLabel}>{medical}</Text>
       </FormLayoutGroup>
-      <FormLayoutGroup top="Единица измерения">
-        <Text style={style.label}>{type}</Text>
+      <FormLayoutGroup top="Назначил врач">
+        <Text style={style.topLabel}>{doctor}</Text>
+      </FormLayoutGroup>
+      <FormLayoutGroup top="Тип">
+        <Text style={style.topLabel}>{type}</Text>
       </FormLayoutGroup>
       <Separator style={style.separator} />
+      <FormLayout>
+        <FormLayoutGroup top="ИНФОРАЦИЯ О ПРИЕМЕ" />
+        <FormLayoutGroup top="Дата начала приема">
+          <Text style={style.mainLabel}>{dstart}</Text>
+        </FormLayoutGroup>
+        <FormLayoutGroup top="Дата окончания приема">
+          <Text style={style.mainLabel}>{dfinish}</Text>
+        </FormLayoutGroup>
+        <List>
+          { times.map((v, index) => (
+
+            <Cell key={index} before={<Text style={style.timeCard.time}>{v.time}</Text>}>
+              <Text style={style.timeCard.dose}>{v.dose}</Text>
+            </Cell>
+          ))}
+        </List>
+      </FormLayout>
     </FormLayout>
   );
 };
 
 AidInfo.propTypes = {
-  name: PropTypes.string.isRequired,
+  user_id: PropTypes.string.isRequired,
+  medical: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
+  doctor: PropTypes.string.isRequired,
+  dstart: PropTypes.instanceOf(Date).isRequired,
+  dfinish: PropTypes.instanceOf(Date).isRequired,
+  times: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 export default AidInfo;
--- src/panels/AidKitPanel.jsx
+++ src/panels/AidKitPanel.jsx
@@ -5,10 +5,12 @@ import {
 } from '@vkontakte/vkui';
 import Icon28MoneyCircleOutline from '@vkontakte/icons/dist/28/money_circle_outline';
 
+import PropTypes from 'prop-types';
 import { ReactComponent as Pill } from '../img/aptechka_28.svg';
+import AidInfo from './AidInfo';
 
 Array.prototype.isEmpty = function () {
-  return this.length == 0;
+  return this.length === 0;
 };
 
 class AidKitPanel extends Component {
@@ -16,27 +18,45 @@ class AidKitPanel extends Component {
     super(props);
     this.state = {
       medicines: [
-
+        {
+          user_id: '404',
+          medical: 'Panadol',
+          doctor: 'Сидоров',
+          type: 'Таблетки',
+          dstart: '2020-06-10',
+          dfinish: '2020-06-25',
+          times: [
+            {
+              dose: '1 таблетка',
+              time: '10:00',
+            },
+          ],
+        },
       ],
     };
   }
 
   render() {
     const { medicines } = this.state;
+    const { goToInfo } = this.props;
 
     return (
       <Group>
         <CardGrid>
           {
                         medicines.map((v) => (
-                          <Card key={v.name} onClick={() => this.props.goToInfo(v)} size="l" mode="shadow">
-                            <Cell before={<Icon28MoneyCircleOutline />} description={v.description}>{v.name}</Cell>
+                          <Card key={v.medical} onClick={() => goToInfo(v)} size="l" mode="shadow">
+                            <Cell
+                              before={<Icon28MoneyCircleOutline />}
+
+                            >
+                              {v.medical}
+                            </Cell>
                           </Card>
                         ))
                     }
         </CardGrid>
         { medicines.isEmpty()
-
           && (
           <Placeholder
             stretched
@@ -52,5 +72,10 @@ class AidKitPanel extends Component {
     );
   }
 }
+
+AidKitPanel.propTypes = {
+  goToInfo: PropTypes.func.isRequired,
+
+};
 
 export default AidKitPanel;
