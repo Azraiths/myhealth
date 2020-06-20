8870609c139f0303d775509e691bfebb944e6d2e
Maba <azraiths@gmail.com>
2020-06-20 20:14:55 +0300

    Second panel connected to backend

--- src/models/addTracking.js
+++ src/models/addTracking.js
@@ -1,16 +1,15 @@
 export default async (data) => {
   const response = await fetch('https://404.kbsu.ru/vkminiapps/api/v1/addtracking.php', {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
     headers: {
       'Content-Type': 'application/json',
-      // 'Content-Type': 'application/x-www-form-urlencoded',
+
     },
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *client
-    body: JSON.stringify(data), // body data type must match "Content-Type" header
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    body: JSON.stringify(data),
   });
-  console.log(response);
-  return response.json(); // parses JSON response into native JavaScript objects
+  return response.json();
 };
